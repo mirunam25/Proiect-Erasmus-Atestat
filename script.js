6cda032b357c63fe0cbd9fc2c14f4ea3c0ce89f8
mirunam25 <mirunam445@gmail.com>
2025-03-15 14:49:50 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,92 @@
+var images = [];
+
+images[0] = ['sun.jpg'];
+images[1] = ['venus.jpg'];
+images[2] = ['mercur.jpg'];
+images[3] = ['marte.jpg'];
+images[4] = ['pamant.jpg'];
+images[5] = ['jupiter.jpg'];
+images[6] = ['neptun.jpg'];
+images[7] = ['uranus.jpg'];
+images[8] = ['moon.jpg'];
+images[9] = ['pluto.jpg'];
+var index = 0;
+
+function change() {
+  document.getElementById("mainPhoto").src = images[index];
+  if (index == 9) {
+    index = 0;
+  } else {
+    index++;
+  }
+
+  setTimeout(change, 2000);
+}
+
+window.onload = change();
+function validateForm()                                    
+{ 
+    var name = document.forms["myForm"]["name"];               
+    var email = document.forms["myForm"]["email"];    
+    var message = document.forms["myForm"]["message"];   
+   
+    if (name.value == "")                                  
+    { 
+        document.getElementById('errorname').innerHTML="Please enter a valid name";  
+        name.focus(); 
+        return false; 
+    }else{
+        document.getElementById('errorname').innerHTML="";  
+    }
+       
+    if (email.value == "")                                   
+    { 
+        document.getElementById('erroremail').innerHTML="Please enter a valid email address"; 
+        email.focus(); 
+        return false; 
+    }else{
+        document.getElementById('erroremail').innerHTML="";  
+    }
+   
+    if (email.value.indexOf("@", 0) < 0)                 
+    { 
+        document.getElementById('erroremail').innerHTML="Please enter a valid email address"; 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (email.value.indexOf(".", 0) < 0)                 
+    { 
+        document.getElementById('erroremail').innerHTML="Please enter a valid email address"; 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (message.value == "")                           
+    {
+        document.getElementById('errormsg').innerHTML="Please enter a valid message"; 
+        message.focus(); 
+        return false; 
+    }else{
+        document.getElementById('errormsg').innerHTML="";  
+    }
+    window.alert("Thank you!");
+    return true; 
+
+}
+
+
+
+  function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
